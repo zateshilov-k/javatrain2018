3490c2a0276c7c9efe494c142853f74a03799898
Zateshilov Konstantin <headon1243@gmail.com>
2018-09-18 19:25:06 +0300

    TrafficLight functionality, TrafficLight tests and TrafficLight usage added.

---
+++ src/TrafficLight.java
@@ -0,0 +1,23 @@
+public class TrafficLight {
+    /* Task:
+       Сделать светофор с выводом цвета в консоль. Вы вводите минуту
+       (от нуля до n) и получаете какой свет горит на светофоре.
+       Первые две минуты красный, потом три минуты желтый и пять минут зеленый.
+    */
+
+    public static String getColorAfterTime(int minutes) {
+        if (minutes < 0) {
+            throw new IllegalArgumentException("Minutes cant be negative.");
+        }
+        int modMinutes = minutes % 10;
+        String result = "";
+        if (modMinutes <= 1) {
+            result = "red";
+        } else if (modMinutes <= 4) {
+            result = "yellow";
+        } else if (modMinutes <= 9) {
+            result = "green";
+        }
+        return result;
+    }
+}
---
+++ src/TrafficLightTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TrafficLightTest {
+    @Test
+    public void colorWorksProperly() {
+        assertEquals("red", TrafficLight.getColorAfterTime(0));
+        assertEquals("red", TrafficLight.getColorAfterTime(1));
+
+        assertEquals("yellow", TrafficLight.getColorAfterTime(2));
+        assertEquals("yellow", TrafficLight.getColorAfterTime(3));
+        assertEquals("yellow", TrafficLight.getColorAfterTime(4));
+
+        assertEquals("green", TrafficLight.getColorAfterTime(5));
+        assertEquals("green", TrafficLight.getColorAfterTime(9));
+
+        assertEquals("red", TrafficLight.getColorAfterTime(10));
+        assertEquals("red", TrafficLight.getColorAfterTime(11));
+
+        assertEquals("yellow", TrafficLight.getColorAfterTime(12));
+        assertEquals("yellow", TrafficLight.getColorAfterTime(13));
+        assertEquals("yellow", TrafficLight.getColorAfterTime(14));
+    }
+
+    @Test
+    public void minutesCantBeNegative() {
+        assertThrows(IllegalArgumentException.class, () -> TrafficLight.getColorAfterTime(-1));
+    }
+}
