4742a02775310dec7fa7fb4f4fdcc54c466b7b35
Zateshilov Konstantin <headon1243@gmail.com>
2018-09-19 09:36:03 +0300

    Added constants for different colors of traffic light and for full cycle. fix comment style

--- src/Task1/TrafficLight.java
+++ src/Task1/TrafficLight.java
@@ -1,23 +1,27 @@
 package Task1;
 
+/* Task:
+ * Сделать светофор с выводом цвета в консоль. Вы вводите минуту
+ * (от нуля до n) и получаете какой свет горит на светофоре.
+ * Первые две минуты красный, потом три минуты желтый и пять минут зеленый.
+ */
 public class TrafficLight {
-    /* Task:
-       Сделать светофор с выводом цвета в консоль. Вы вводите минуту
-       (от нуля до n) и получаете какой свет горит на светофоре.
-       Первые две минуты красный, потом три минуты желтый и пять минут зеленый.
-    */
+    static final int FULL_CYCLE_TIME = 10;
+    static final int RED_TIME = 2;
+    static final int YELLOW_TIME = 3;
+    static final int GREEN_TIME = 5;
 
     public static String getColorAfterTime(int minutes) {
         if (minutes < 0) {
             throw new IllegalArgumentException("Minutes cant be negative.");
         }
-        int modMinutes = minutes % 10;
+        int modMinutes = minutes % FULL_CYCLE_TIME;
         String result = "";
-        if (modMinutes <= 1) {
+        if (modMinutes < RED_TIME) {
             result = "red";
-        } else if (modMinutes <= 4) {
+        } else if (modMinutes < RED_TIME + YELLOW_TIME) {
             result = "yellow";
-        } else if (modMinutes <= 9) {
+        } else if (modMinutes < RED_TIME + YELLOW_TIME + GREEN_TIME) {
             result = "green";
         }
         return result;
