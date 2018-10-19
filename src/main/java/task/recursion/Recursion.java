a59cdf71379ad4380bd8035078b836ed6949ca2e
Zateshilov Konstantin <headon1243@gmail.com>
2018-10-19 15:04:22 +0300

    Add runner for recursion

--- src/main/java/task/recursion/Recursion.java
+++ src/main/java/task/recursion/Recursion.java
@@ -1,28 +1,55 @@
 package main.java.task.recursion;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+
 public class Recursion {
-    public static long factorial(int n) {
-        if (n < 0 ) {
+    private final Map<String, Function<Integer,Long>> unaryMethods = new HashMap<>();
+    private final Map<String, BiFunction<Double, Integer, Double>> biMethods = new HashMap<>();
+
+    public Recursion() {
+        unaryMethods.put("Factorial", this::factorial);
+        unaryMethods.put("Sum all digits of number", this::sumDigits);
+        unaryMethods.put("Count all digits of number", this::countDigits);
+        biMethods.put("Power of double", this::pow);
+    }
+
+    public Map<String, BiFunction<Double, Integer, Double>> getBiMethods() {
+        return new HashMap<>(biMethods);
+    }
+
+    public Map<String, Function<Integer, Long>> getUnaryMethods() {
+        return new HashMap<>(unaryMethods);
+    }
+
+    public long factorial(int n) {
+        if (n < 0) {
             throw new IllegalArgumentException("n should be natural");
         }
         if (n == 0) {
             return 1;
         } else {
-            return n * factorial(n-1);
+            return n * factorial(n - 1);
         }
     }
-    public static double pow(double num, int pow) {
-        if (pow < 0 ) {
+
+    public double pow(double num, int pow) {
+        if (pow < 0) {
             throw new IllegalArgumentException("pow should be natural");
         }
         if (pow == 0) {
             return 1;
         } else {
-            return num*pow(num,pow-1);
+            return num * pow(num, pow - 1);
         }
     }
-    public static int sumDigits(int num) {
-        if (num < 0 ) {
+
+    public long sumDigits(int num) {
+        if (num < 0) {
             throw new IllegalArgumentException("n should be natural");
         }
         if (num / 10 == 0) {
@@ -32,14 +59,15 @@ public class Recursion {
         }
 
     }
-    public static int countDigits(int num) {
-        if (num < 0 ) {
+
+    public long countDigits(int num) {
+        if (num < 0) {
             throw new IllegalArgumentException("n should be natural");
         }
         if (num / 10 == 0) {
             return 1;
         } else {
-            return (1 + countDigits(num/10));
+            return (1 + countDigits(num / 10));
         }
     }
 }
--- src/test/java/task/recursion/RecursionTest.java
+++ src/test/java/task/recursion/RecursionTest.java
@@ -1,52 +1,53 @@
 package test.java.task.recursion;
 
+import main.java.task.recursion.Recursion;
 import org.junit.Test;
 
 import static main.java.task.recursion.Recursion.*;
 import static org.junit.Assert.assertEquals;
 
 public class RecursionTest {
+    Recursion recursion = new Recursion();
     @Test
     public void testFactorial() {
-        assertEquals(1, factorial(0));
-        assertEquals(24, factorial(4));
-        assertEquals(3_628_800, factorial(10));
-        assertEquals(62_270_208_00l, factorial(13));
+        assertEquals(1, recursion.factorial(0));
+        assertEquals(24, recursion.factorial(4));
+        assertEquals(3_628_800, recursion.factorial(10));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testFactorial_throws() {
-        factorial(-1);
+        recursion.factorial(-1);
     }
 
     @Test
     public void testPow() {
-        assertEquals(27, pow(3, 3), 10E-10);
-        assertEquals(1.44, pow(1.2, 2), 10E-10);
+        assertEquals(27, recursion.pow(3, 3), 10E-10);
+        assertEquals(1.44, recursion.pow(1.2, 2), 10E-10);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testPow_throws() {
-        pow(3, -1);
+        recursion.pow(3, -1);
     }
 
     @Test
     public void testSumDigits() {
-        assertEquals(3, sumDigits(101010));
-        assertEquals(15, sumDigits(12345));
+        assertEquals(3, recursion.sumDigits(101010));
+        assertEquals(15, recursion.sumDigits(12345));
     }
     @Test(expected = IllegalArgumentException.class)
     public void testSumDigits_throws() {
-        sumDigits(-1);
+        recursion.sumDigits(-1);
     }
 
     @Test
     public void testCountDigits() {
-        assertEquals(6, countDigits(101010));
-        assertEquals(5, countDigits(12345));
+        assertEquals(6, recursion.countDigits(101010));
+        assertEquals(5, recursion.countDigits(12345));
     }
     @Test(expected = IllegalArgumentException.class)
     public void testCountDigits_throws() {
-        countDigits(-2);
+        recursion.countDigits(-2);
     }
 }
